45508757ca39e53caf74b355c6259a73e95653f6
efrat gavriel <efratg@darkeychana.com>
2022-12-02 11:00:15 +0200

    my commit message

---
+++ CLOUD/CLOUD/controlers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+
+namespace CLOUD.controlers
+{
+ [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public async Task<int> DoSomething(int x)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync($"http://127.0.0.1:5000/do?x={x}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.Write("Success");
+                    }
+                    else
+                    {
+                        Console.Write("Failure");
+                    }
+
+                    return int.Parse(await response.Content.ReadAsStringAsync());
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
+
+
